55686880074be7b6ae60e013444dfa5fb3e9cfbf
Sebastien CLOLUS <sclolus@e3r1p13.42.fr>
2017-09-19 04:02:35 +0200

    strlen du turfu lel

---
+++ asm/includes/colors.h
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   colors.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sclolus <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/18 22:41:21 by sclolus           #+#    #+#             */
+/*   Updated: 2017/09/19 01:14:06 by sclolus          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COLORS_H
+
+# define COLORS_H
+# define RESET "\x1B[0m"
+# define BOLD "\x1B[1m"
+# define BOLD_OFF "\x1B[22m"
+# define ITALICS "\x1B[3m"
+# define ITALICS_OFF "\x1B[23m"
+# define UNDERLINE "\x1B[4m"
+# define UNDERLINE_OFF "\x1B[24m"
+# define INVERSE "\x1B[7m"
+# define INVERSE_OFF "\x1B[27m"
+# define STRIKETHROUGH "\x1B[9m"
+# define STRIKETHROUGH_OFF "\x1B[29m"
+
+# define RED_FOND "\x1B[31m"
+# define GREEN_FOND "\x1B[32m"
+# define YELLOW_FOND "\x1B[33m"
+# define BLUE_FOND "\x1B[34m"
+# define MAGENTA_FOND "\x1B[35m"
+# define CYAN_FOND "\x1B[36m"
+# define RESET_FOND "\x1B[39m"
+
+# define BACK_BLACK "\x1B[40m"
+# define BACK_RED "\x1B[41m"
+# define BACK_GREEN "\x1B[42m"
+# define BACK_YELLOW "\x1B[43m"
+# define BACK_BLUE "\x1B[44m"
+# define BACK_MAGENTA "\x1B[45m"
+# define BACK_CYAN "\x1B[46m"
+# define BACK_WHITE "\x1B[47m"
+# define BACK_RESET "\x1B[49m"
+
+#endif
