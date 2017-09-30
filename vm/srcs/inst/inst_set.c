41a127c0a36b68142cc05be656f1265fd52518d0
Asya CORBEAU <acorbeau@e1r12p10.42.fr>
2017-09-30 04:25:35 +0200

    Fix sti ?

--- vm/srcs/inst/inst_set.c
+++ vm/srcs/inst/inst_set.c
@@ -6,7 +6,7 @@
 /*   By: acorbeau <acorbeau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/14 21:20:49 by acorbeau          #+#    #+#             */
-/*   Updated: 2017/09/26 09:18:19 by acorbeau         ###   ########.fr       */
+/*   Updated: 2017/09/30 04:25:07 by acorbeau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ t_int32		inst_st(t_vm *vm, t_byte ci, t_process *pc)
 	if (IP2(pc->inst[1]) == T_REG)
 		pc->reg[pc->inst[3] - 1] = param_dirval(pc, 1);
 	else
-		mem_writeint(vm, param_dirval(pc, 1),
+		mem_writeint(vm, param_idxval(vm, pc, 1),
 						IDXPTR(pc->cc, param_dirval(pc, 2)), ci + 1);
 	return (1);
 }
